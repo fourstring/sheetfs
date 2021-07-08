b7cf9f0918167278117a7f7566b6d1679a805892
fourstring <i@zby.io>
2021-07-08 15:08:57 +0800

    fix: incorrect initial version of chunks holding MetaCell

--- fsclient/client_test.go
+++ fsclient/client_test.go
@@ -338,6 +338,15 @@ func TestReadAndWrite(t *testing.T) {
 			So(read[:len(b)], ShouldResemble, b)
 			So(size, ShouldEqual, 2048)
 			So(err, ShouldBeNil)
+
+			size, err = file.WriteAt(ctx, b, MetaCellRow, MetaCellCol, " ")
+			So(size, ShouldEqual, len(b))
+			So(err, ShouldBeNil)
+
+			size, err = file.ReadAt(ctx, read, MetaCellRow, MetaCellCol)
+			So(read[:len(b)], ShouldResemble, b)
+			So(size, ShouldEqual, 8192)
+			So(err, ShouldBeNil)
 		})
 		// stop nodes unconditionally
 		Reset(func() {
