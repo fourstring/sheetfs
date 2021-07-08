21c4bdb63c1085775b852f27c6cf6645b3e66bd6
fourstring <i@zby.io>
2021-07-08 10:41:54 +0800

    feat: add common election support.

---
+++ election/example/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/fourstring/sheetfs/election"
+	"log"
+	"time"
+)
+
+var electionZnode = "/test_election"
+var electionPrefix = "4da1fce7-d3f8-42dd-965d-4c3311661202-n_"
+var electionAck = "/test_election_ack"
+var electionServers = []string{
+	"127.0.0.1:2181",
+	"127.0.0.1:2182",
+	"127.0.0.1:2183",
+}
+var id = flag.String("i", "", "ID of proposer")
+
+func main() {
+	flag.Parse()
+	e, err := election.NewElector(electionServers, 1*time.Second, electionZnode, electionPrefix, electionAck)
+	proposal, err := e.CreateProposal()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s: My proposal is %s\n", *id, proposal)
+	for {
+		success, watch, notify, err := e.TryBeLeader()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if success {
+			break
+		}
+		fmt.Printf("%s: I'm secondary and watching %s\n", *id, watch)
+		<-notify
+	}
+	fmt.Printf("%s: I'm primary!\n", *id)
+	err = e.AckLeader(*id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		time.Sleep(1 * time.Minute)
+	}
+}
--- master/config/config.go
+++ master/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"math"
+	"time"
 )
 
 const (
@@ -11,9 +12,18 @@ const (
 	DBName           = "master.db"
 	SheetMetaCellRow = uint32(math.MaxUint32)
 	SheetMetaCellCol = uint32(math.MaxUint32)
+	ElectionZnode    = "/election"
+	ElectionAck      = "/election_ack"
+	ElectionChild    = "/election/a20ffeb5-319a-4e0b-b54d-646fb93d3158-n_"
+	ElectionTimeout  = 5 * time.Second
 )
 
 var SheetMetaCellID = int64(0)
+var ElectionServers = []string{
+	"127.0.0.1:2181",
+	"127.0.0.1:2182",
+	"127.0.0.1:2183",
+}
 
 func init() {
 	SheetMetaCellID = -1
