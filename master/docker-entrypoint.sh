a97ce3451cf7a028bc8c015e43114c55abbf3b4d
fourstring <i@zby.io>
2021-07-14 10:10:37 +0800

    deployment: update docker-compose configuration to include master replication, zookeepers and kafka

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-./master -a "$MASTER_ADDR"
+./master -a "$MASTER_ADDR_FOR_CLIENT" -dngroups "$DATANODE_GROUPS" -elack "$ELECTION_ACK" -ekznode "$ELECTION_ZNODE" \
+-i "$NODE_ID" -kfserver "$KAFKA_SERVER" -kftopic "$KAFKA_TOPIC" -p "$MASTER_PORT" -zkservers "$ZOOKEEPER_SERVERS"
